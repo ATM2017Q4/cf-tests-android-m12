7971376ede262253d1b799ece872c3b5ddf41cb1
Tatsiana Marfel <tatsiana_marfel@epam.com>
2018-02-26 18:54:43 +0300

    Revert "Remove commented code"
    This reverts commit 7ec73178e24f29aa43de92f04c1736e544526bbb.

--- src/test/java/com/cf/ui/tests/CheapFlightsTest.java
+++ src/test/java/com/cf/ui/tests/CheapFlightsTest.java
@@ -52,7 +52,14 @@ public class CheapFlightsTest {
     }
 
     @Test
-    public void searchCheapFlight() {
+    public void searchCheapFlight() throws InterruptedException {
+//        while (driver.findElementsById("com.cf.flightsearch:id/closeBtn").isEmpty()) {
+//            try {
+//                driver.findElementsById("com.cf.flightsearch:id/closeBtn").get(0);
+//            } catch (java.lang.IndexOutOfBoundsException e) {
+//                System.out.println("Overlay hasn't appeared yet");
+//            }
+//        }
         LoadPage loadPage = new LoadPage(driver);
 
         loadPage.closeSignInOverlay()
@@ -60,6 +67,53 @@ public class CheapFlightsTest {
                 .chooseDestination("London")
                 .pickDates("Jun", 7, 17)
                 .submitForm();
+
+
+//        new FluentWait(driver).withTimeout(10, TimeUnit.SECONDS).ignoring(org.openqa.selenium.NoSuchElementException.class).pollingEvery(1, TimeUnit.SECONDS).until(ExpectedConditions.presenceOfElementLocated(By.id("com.cf.flightsearch:id/closeBtn")));
+//        driver.findElementById("com.cf.flightsearch:id/closeBtn").click();
+//        driver.findElementById("com.cf.flightsearch:id/originCode").click();
+////        driver.findElementById("com.cf.flightsearch:id/text").click();
+//        new FluentWait(driver).withTimeout(10, TimeUnit.SECONDS).pollingEvery(1, TimeUnit.SECONDS).ignoring(org.openqa.selenium.NoSuchElementException.class).until(ExpectedConditions.visibilityOf(driver.findElementById("com.cf.flightsearch:id/smartySearchText")));
+////        new TouchAction(driver).tap(driver.findElementById("com.cf.flightsearch:id/smartySearchText"));
+//        driver.findElementById("com.cf.flightsearch:id/smartySearchText").sendKeys("Vienna");
+//        new FluentWait(driver).withTimeout(10, TimeUnit.SECONDS).pollingEvery(1, TimeUnit.SECONDS).until(ExpectedConditions.visibilityOf(driver.findElement(new MobileBy.ByAndroidUIAutomator("className(android.widget.LinearLayout).instance(4)"))));
+//        driver.findElement(new MobileBy.ByAndroidUIAutomator("className(android.widget.LinearLayout).instance(4)")).click();
+//        driver.findElementById("com.cf.flightsearch:id/destinationCode").click();
+//        driver.findElementById("com.cf.flightsearch:id/smartySearchText").sendKeys("London");
+//        new FluentWait(driver).withTimeout(10, TimeUnit.SECONDS).pollingEvery(1, TimeUnit.SECONDS).until(ExpectedConditions.visibilityOf(driver.findElement(new MobileBy.ByAndroidUIAutomator("className(android.widget.LinearLayout).instance(4)"))));
+//        driver.findElement(new MobileBy.ByAndroidUIAutomator("className(android.widget.LinearLayout).instance(4)")).click();
+//        driver.findElementById("com.cf.flightsearch:id/dates").click();
+//        Thread.sleep(5000);
+//        new FluentWait(driver).withTimeout(10, TimeUnit.SECONDS)
+//                .pollingEvery(1, TimeUnit.SECONDS)
+//                .until(ExpectedConditions.visibilityOf(driver.findElement(new MobileBy.ByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().textContains(\"May\"))"))));
+//        driver.findElements(new MobileBy.ByAndroidUIAutomator("new UiSelector().className(\"android.widget.RelativeLayout\")")).get(15).click();
+//        driver.findElements(new MobileBy.ByAndroidUIAutomator("new UiSelector().className(\"android.widget.RelativeLayout\")")).get(25).click();
+//        driver.findElementById("com.cf.flightsearch:id/applyButton").click();
+//        driver.findElementById("com.cf.flightsearch:id/searchImage").click();
+//        new FluentWait(driver).withTimeout(60, TimeUnit.SECONDS)
+//                .pollingEvery(1, TimeUnit.SECONDS)
+//                .ignoring(org.openqa.selenium.NoSuchElementException.class)
+//                .until(ExpectedConditions.invisibilityOfElementLocated(By.id("com.cf.flightsearch:id/animationView")));
+//        new FluentWait(driver).withTimeout(60, TimeUnit.SECONDS)
+//                .pollingEvery(1, TimeUnit.SECONDS)
+//                .ignoring(org.openqa.selenium.NoSuchElementException.class)
+//                .until(ExpectedConditions.invisibilityOfElementLocated(By.id("com.cf.flightsearch:id/progressIndicator")));
+//        driver.findElementById("com.cf.flightsearch:id/filtersCard").click();
+//        driver.findElementById("com.cf.flightsearch:id/oneStop").click();
+////        driver.findElement(new MobileBy.ByAndroidUIAutomator("new UiScrollable(new UiSelector().resourceId(\"com.cf.flightsearch:id/scrollRoot\")).setAsVerticalList().scrollIntoView(new UiSelector().textContains(\"Duration\"))"));
+//        TouchAction action = new TouchAction(driver);
+//        action.press(driver.findElementById("com.cf.flightsearch:id/averagePriceLayout")).waitAction(Duration.ofSeconds(2)).moveTo(driver.findElementById("com.cf.flightsearch:id/toolbar")).release().perform();
+//        action.press(driver.findElementById("com.cf.flightsearch:id/graph")).waitAction(Duration.ofSeconds(2)).moveTo(driver.findElementById("com.cf.flightsearch:id/toolbar")).release().perform();
+//        Thread.sleep(5000);
+//        driver.findElement(new MobileBy.ByAndroidUIAutomator("new UiSelector().textContains(\"Duration\")")).click();
+//        MobileElement slider = driver.findElement(new MobileBy.ByAndroidUIAutomator("new UiSelector().resourceId(\"com.cf.flightsearch:id/layoverSlider\").instance(0)"));
+//        Dimension size = slider.getSize();
+//        int sliderWidth = size.getWidth();
+//        action.press(slider).moveTo((sliderWidth / 4), 0).release().perform();
+//        driver.findElementById("com.cf.flightsearch:id/applyButton").click();
+//        String[] price = driver.findElementsById("com.cf.flightsearch:id/price").get(0).getText().split("\\$");
+//        Assertions.assertTrue(Integer.parseInt(price[1]) < 200);
 
     }
 
